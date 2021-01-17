f664b235f9fbc2a7db870a4105142ce5216b648b
rmur84 <72394245+rmur84@users.noreply.github.com>
2021-01-17 10:15:37 +0700

    Update script.js

--- classwork1/script.js
+++ classwork1/script.js
@@ -1,4 +1,5 @@
 var value1 = 'Hello';
 var value3 = 'world';
-var value4 = value1 + ' ' + 'Hello' + ' '  + value3;
+var value4 = value1 + ' ' + value3;
+value4 = value1 + ' ' + value1 + ' ' + value3;
 console.log (value4);
